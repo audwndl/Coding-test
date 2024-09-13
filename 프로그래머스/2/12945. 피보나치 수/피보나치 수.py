16a6dfa5cb54de8fb50adfbcee09de2b5fe10725
이주명 <153901666+audwndl@users.noreply.github.com>
2024-09-13 18:26:43 +0900

    [level 2] Title: 피보나치 수, Time: 471.61 ms, Memory: 456 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = []
+    for i in range(n+1):
+        if i==0 or i==1:
+            answer.append(i)
+        else:
+            answer.append(answer[i-1] + answer[i-2])
+    return answer[-1] % 1234567
